8ba5522cfeebac57a0d797961f52f360443da0cb
ZapadniyBot <enotenotovic186@gmail.com>
2026-08-14 17:37:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Заглушка для пустых кнопок
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('click', function(e) {
-        // Если у кнопки вместо ссылки стоит знак #
-        if (this.getAttribute('href') === '#') {
-            e.preventDefault();
-            alert('Ссылка временно недоступна или проверяется администратором.');
-        }
-    });
-});
+
